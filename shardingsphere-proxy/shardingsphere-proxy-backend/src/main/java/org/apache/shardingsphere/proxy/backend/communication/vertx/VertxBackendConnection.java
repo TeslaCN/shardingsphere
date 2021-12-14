91bb3e7bf40628436dc9262ece186d8db33fe7f9
吴伟杰 <wuweijie@apache.org>
2021-12-14 19:26:46 +0800

    Implements local transaction for reactive backend

--- shardingsphere-proxy/shardingsphere-proxy-backend/src/main/java/org/apache/shardingsphere/proxy/backend/communication/vertx/VertxBackendConnection.java
+++ shardingsphere-proxy/shardingsphere-proxy-backend/src/main/java/org/apache/shardingsphere/proxy/backend/communication/vertx/VertxBackendConnection.java
@@ -34,6 +34,7 @@ import org.apache.shardingsphere.infra.executor.sql.prepare.driver.vertx.Executo
 import org.apache.shardingsphere.infra.executor.sql.prepare.driver.vertx.VertxExecutionContext;
 import org.apache.shardingsphere.proxy.backend.communication.BackendConnection;
 import org.apache.shardingsphere.proxy.backend.communication.jdbc.connection.ConnectionPostProcessor;
+import org.apache.shardingsphere.proxy.backend.communication.vertx.transaction.VertxLocalTransactionManager;
 import org.apache.shardingsphere.proxy.backend.context.ProxyContext;
 import org.apache.shardingsphere.proxy.backend.session.ConnectionSession;
 
@@ -118,25 +119,51 @@ public final class VertxBackendConnection implements BackendConnection<Future<Vo
     
     @Override
     public Future<Void> prepareForTaskExecution() {
+        if (!connectionSession.isAutoCommit() && !connectionSession.getTransactionStatus().isInTransaction()) {
+            VertxLocalTransactionManager transactionManager = new VertxLocalTransactionManager(this);
+            return transactionManager.begin();
+        }
         return Future.succeededFuture();
     }
     
     @Override
     public Future<Void> closeExecutionResources() {
-        return closeAllConnections();
+        if (!connectionSession.getTransactionStatus().isInTransaction()) {
+            return closeAllConnections(false);
+        }
+        return Future.succeededFuture();
     }
     
     @Override
     public Future<Void> closeAllResources() {
-        return closeAllConnections();
+        return closeAllConnections(true);
     }
     
     @SuppressWarnings("rawtypes")
-    private Future<Void> closeAllConnections() {
-        List<Future> closeFutures = new ArrayList<>(cachedConnections.values().size());
-        for (Future<SqlConnection> each : cachedConnections.values()) {
-            closeFutures.add(each.compose(SqlClient::close));
+    private Future<Void> closeAllConnections(final boolean rollbackBeforeClosing) {
+        Collection<Future<SqlConnection>> connections = cachedConnections.values();
+        if (connections.isEmpty()) {
+            return Future.succeededFuture();
+        }
+        List<Future> closeFutures = new ArrayList<>(connections.size());
+        for (Future<SqlConnection> each : connections) {
+            closeFutures.add(rollbackBeforeClosing ? each.compose(connection -> connection.query("rollback").execute().compose(unused -> connection.close())) : each.compose(SqlClient::close));
         }
         return CompositeFuture.join(closeFutures).onComplete(unused -> cachedConnections.clear()).compose(unused -> Future.succeededFuture());
+    }
+    
+    /**
+     * Execute in all cached connections.
+     *
+     * @param sql sql to be executed
+     * @return join future
+     */
+    @SuppressWarnings("rawtypes")
+    public Future<Void> executeInAllCachedConnections(final String sql) {
+        List<Future> futures = new ArrayList<>(cachedConnections.size());
+        for (Future<SqlConnection> each : cachedConnections.values()) {
+            futures.add(each.compose(connection -> connection.query(sql).execute()));
+        }
+        return CompositeFuture.join(futures).compose(result -> Future.succeededFuture());
     }
 }
