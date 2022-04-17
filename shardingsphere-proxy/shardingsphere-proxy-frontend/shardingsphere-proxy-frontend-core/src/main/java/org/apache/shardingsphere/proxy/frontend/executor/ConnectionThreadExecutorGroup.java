a7f6578c252953ed43504cca1bf4fa94b279de63
吴伟杰 <wuweijie@apache.org>
2022-04-17 17:26:30 +0800

    Using VirtualThread in ConnectionThreadExecutorGroup

--- shardingsphere-proxy/shardingsphere-proxy-frontend/shardingsphere-proxy-frontend-core/src/main/java/org/apache/shardingsphere/proxy/frontend/executor/ConnectionThreadExecutorGroup.java
+++ shardingsphere-proxy/shardingsphere-proxy-frontend/shardingsphere-proxy-frontend-core/src/main/java/org/apache/shardingsphere/proxy/frontend/executor/ConnectionThreadExecutorGroup.java
@@ -23,8 +23,7 @@ import lombok.NoArgsConstructor;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -61,7 +60,7 @@ public final class ConnectionThreadExecutorGroup {
     }
     
     private ExecutorService newSingleThreadExecutorService(final int connectionId) {
-        return new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(), runnable -> new Thread(runnable, String.format("Connection-%d-ThreadExecutor", connectionId)));
+        return Executors.newThreadPerTaskExecutor(Thread.ofVirtual().name("Connection-" + connectionId + "-VirtualThread").factory());
     }
     
     /**
